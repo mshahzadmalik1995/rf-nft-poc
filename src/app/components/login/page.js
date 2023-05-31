2511ca30a5bf5ea2fa87d3562aa0aa4981f6f920
Tushar Gupta <tushar.gupta01@nagarro.com>
2023-05-31 11:15:18 +0530

    Changes : Addded login and user register

---
+++ src/app/components/login/page.js
@@ -0,0 +1,126 @@
+"use client";
+import React, { useState } from "react";
+import bgImage from '../images/image.jpg'
+import Link from 'next/link';
+
+import {
+    Avatar,
+    Button,
+    Box,
+    Container,
+    CssBaseline,
+    TextField,
+    Typography,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        //marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "40%",
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    backgroundImage: {
+        backgroundImage: `url(${bgImage.src})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "120vh",
+    },
+}));
+
+const Login = () => {
+    const classes = useStyles();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(bgImage);
+    };
+
+    return (
+        <Box className={classes.backgroundImage}>
+            <Container component="main">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h4">
+                        Royal Enfield Login
+                    </Typography>
+                    <form className={classes.form} onSubmit={handleSubmit}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoComplete="username"
+                            autoFocus
+                            value={username}
+                            onChange={handleUsernameChange}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+
+                        <div>
+                            <Link href="/components/register">
+                                <h1 style={{ textAlign: "right" }}>
+                                    Register User
+                                </h1>
+                            </Link>
+                        </div>
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                    </form>
+                </div>
+            </Container >
+        </Box >
+    );
+};
+
+export default Login;
