1a17aff9c6b9a0391bdaf3409b5a813a2472ca26
Vishal <vish88.gpt@gmail.com>
2023-05-31 20:50:24 +0530

    Realigning the image and adding disclaimer

--- src/app/components/register/page.js
+++ src/app/components/register/page.js
@@ -26,19 +26,19 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "40%",
+    width: "90%",
     marginTop: theme.spacing(1),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-  backgroundImage: {
-    backgroundImage: `url(${bgImage.src})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "120vh",
-  },
+  // backgroundImage: {
+  //   backgroundImage: `url(${bgImage.src})`,
+  //   backgroundRepeat: "no-repeat",
+  //   backgroundSize: "top",
+  //   backgroundPosition: "center",
+  //   height: "20vh",
+  // },
 }));
 
 const Register = () => {
@@ -53,6 +53,7 @@ const Register = () => {
     // Handle user registration logic here
   };
 
+  /*
   return (
     <Box className={classes.backgroundImage}>
       <Container component="main">
@@ -118,6 +119,16 @@ const Register = () => {
               onChange={(e) => setPincode(e.target.value)}
             />
             <div>
+              <br></br>
+              <label>
+                <b>Disclaimer:</b> By signing this form/checking this box, you acknowledge and agree that we may use the information you share with us, to communicate with you through e-mails, text messages, WhatsApp and calls, in order to provide our product or service related information and/or for promotional and marketing purposes. All information provided will be secured and processed as per our privacy policy. 
+              </label>
+              <br></br>
+              <br></br>
+              <input type="checkbox" id="disclaimer" name="disclaimer"/>
+              <label>    I accept the terms and conditions as well as the privacy policy </label> 
+            </div>
+            <div>
               <Link href="/components/login">
                 <h1 style={{ textAlign: "right" }}>
                   Back to Sign in
@@ -139,5 +150,103 @@ const Register = () => {
     </Box>
   );
 };
+*/
 
+  return (
+    <Box>
+      <Container component="main">
+        <CssBaseline />
+        <img src="/RE1.jpg" alt="Royal enfield" className="w-96 h-50 rounded-lg"/>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h4">
+            Royal Enfield Register
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="fullname"
+              label="Full Name"
+              name="fullname"
+              autoComplete="Full Name"
+              autoFocus
+              value={fullname}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Id"
+              name="email"
+              autoComplete="Email id"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="contactNo"
+              label="Contact Number"
+              name="contactNo"
+              autoComplete="Contact Number"
+              autoFocus
+              value={contactNo}
+              onChange={(e) => setContactNo(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="pincode"
+              label="Pincode"
+              type="pincode"
+              id="pincode"
+              autoComplete="Pincode"
+              value={pincode}
+              onChange={(e) => setPincode(e.target.value)}
+            />
+            <div>
+              <br></br>
+              <label>
+                <b>Disclaimer:</b> By signing this form/checking this box, you acknowledge and agree that we may use the information you share with us, to communicate with you through e-mails, text messages, WhatsApp and calls, in order to provide our product or service related information and/or for promotional and marketing purposes. All information provided will be secured and processed as per our privacy policy. 
+              </label>
+              <br></br>
+              <br></br>
+              <input type="checkbox" id="disclaimer" name="disclaimer"/>
+              <label>    I accept the terms and conditions as well as the privacy policy </label> 
+            </div>
+            <div>
+              <Link href="/components/login">
+                <h1 style={{ textAlign: "right" }}>
+                  Back to Sign in
+                </h1>
+              </Link>
+            </div>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Register
+            </Button>
+          </form>
+        </div>
+      </Container>
+    </Box>
+  );
+};
 export default Register;
