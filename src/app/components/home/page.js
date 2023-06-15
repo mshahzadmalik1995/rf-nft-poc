6b438cb2cd11fb30228b8a709f53465614208f43
Vishal <vish88.gpt@gmail.com>
2023-06-15 09:05:12 +0530

    hiding My Missions if no missions enrolled

--- src/app/components/home/page.js
+++ src/app/components/home/page.js
@@ -2,20 +2,59 @@
 
 import sampleData from "@/app/data/data";
 import { useEffect, useState, useContext } from "react";
+//import React from "react";
+//import "react-step-progress-bar/styles.css";
+//import { ProgressBar, Step } from "react-step-progress-bar";
 import MissionCard from "../missioncard";
 import Header from "../header/page";
 import MyContext from "@/app/context/mycontext";
 
+// class StepProgressBar extends React.Component {
+//     render() {
+//       return (
+//         <ProgressBar
+//           percent={75}
+//           filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+//         >
+//           <Step transition="scale">
+//             {({ accomplished }) => (
+//               <img
+//                 style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+//                 width="30"
+//                 src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+//               />
+//             )}
+//           </Step>
+//           <Step transition="scale">
+//             {({ accomplished }) => (
+//               <img
+//                 style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+//                 width="30"
+//                 src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/97/Pikachu_%28Smiling%29.png/revision/latest?cb=20170410234508"
+//               />
+//             )}
+//           </Step>
+//           <Step transition="scale">
+//             {({ accomplished }) => (
+//               <img
+//                 style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+//                 width="30"
+//                 src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+//               />
+//             )}
+//           </Step>
+//         </ProgressBar>
+//       );
+//     }
+//   }
+
+
 const Home = () => {
-    //const [data, setData] = useState(sampleData);
-    //const [userId, setUserId] = useState();
     const [data, setData] = useState();
     const [dataUser, setDataUser] = useState();
-    //const {userLoginData} = useContext(MyContext);
     const userData = localStorage.getItem("myUserState");
     const parsedUserData = JSON.parse(userData);
-    //setUserId(parsedUserData._id);
-   // console.log(`userLoginData ${userLoginData.username}`)
+    const [showMyMissions, setShowMyMissions] = useState();
 
     useEffect(() => {
         const getMission = async () => {
@@ -41,23 +80,19 @@ const Home = () => {
         getMission();
 
         const getUserMission = async () => {
-
             try{
-
                 const response = await fetch(`/api/getuserassociatemission?userId=${parsedUserData._id}`, {
                     method: 'GET',
                     headers: {"Content_Type":"application/json"},
                     
                 })
                 const data = await response.json()
-                setDataUser(data.userMission);
-                console.log(data)
-                if(response.status === 200){
-                 //  router.push("/components/home")
-                 //setData(data);
-                 console.log("inside if block of user missions")
-                } else {
-                    console.log("no data found while fetching user mission!")
+                if(Object.keys(data.userMission).length != 0 && response.status === 200){
+                    setDataUser(data.userMission);
+                }
+                else{
+                    setShowMyMissions("hidden")
+                    console.log("no data found while fetching user mission!.")
                 }
             } catch(e) {
                 console.error('Error fetching data in fetching user mission :', error);
@@ -71,7 +106,7 @@ const Home = () => {
     return (
         <div className="flex flex-col gap-2 relative">
         <Header />
-        <div className="mt-10">
+        <div className="mt-10" hidden={showMyMissions}>
             <h1 className="text-black-400 font-bold p-2 text-2xl">My Missions</h1>
             <div className="flex absolute flex-wrap p-2 gap-2 mt-4">
                 {
@@ -80,18 +115,18 @@ const Home = () => {
                     })
                 }
             </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
         </div>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
             <div className="mt-10">
                 <h1 className="text-black-400 font-bold p-2 text-2xl">All Missions</h1>
                 <div className="flex absolute flex-wrap p-2 gap-2 mt-4">
